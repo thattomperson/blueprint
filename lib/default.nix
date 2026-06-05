3cb652de87def736dc0973740d3529fb3d6b9e7e
Thomas Albrighton <thomasalbrighton@gmail.com>
2026-06-05 14:50:42 +0930

    Test

--- lib/default.nix
+++ lib/default.nix
@@ -319,7 +319,6 @@ in rec {
               username,
               modulePath,
               pkgs,
-              system,
             }:
             home-manager.lib.homeManagerConfiguration {
               inherit pkgs;
